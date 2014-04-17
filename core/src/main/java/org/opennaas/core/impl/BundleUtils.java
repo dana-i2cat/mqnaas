2ccca371dc5c9f109f2eeea0b8388d0f89be1dca
logoff <juliocarlos.barrera@i2cat.net>
2014-04-17 11:34:29 +0200

    Classpath-scanning improved filtering by target annotations package.

--- core/src/main/java/org/opennaas/core/impl/BundleUtils.java
+++ core/src/main/java/org/opennaas/core/impl/BundleUtils.java
@@ -1,11 +1,18 @@
 package org.opennaas.core.impl;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryPoolMXBean;
+import java.lang.management.MemoryType;
+import java.lang.management.MemoryUsage;
 import java.lang.reflect.Modifier;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import org.apache.commons.lang3.ClassUtils;
 import org.osgi.framework.Bundle;
+import org.osgi.framework.wiring.BundleRevision;
+import org.osgi.framework.wiring.BundleWire;
 import org.osgi.framework.wiring.BundleWiring;
 
 public abstract class BundleUtils {
@@ -23,9 +30,14 @@ public abstract class BundleUtils {
 		ImplementationDetectingClassVisitor<T> visitor = new ImplementationDetectingClassVisitor<T>(interfaceScannedFor);
 
 		for (String resourceName : wiring.listResources("/", "*.class", BundleWiring.LISTRESOURCES_RECURSE)) {
+			System.out.println("Resource to be scanned: " + resourceName);
 			try {
 				// Load the classes and pass them to the visitor
 				resourceName = resourceName.replaceAll(".class", "").replaceAll("/", ".");
+
+				// FIXME use low trace level log or remove these lines
+				// printMemoryUsage();
+				// System.out.println("Loading class " + resourceName);
 
 				visitor.visit(bundle.loadClass(resourceName));
 			} catch (IncompatibleClassChangeError e) {
@@ -79,6 +91,43 @@ public abstract class BundleUtils {
 		public Set<Class<? extends T>> getImplementations() {
 			return implementations;
 		}
+	}
+
+	public static boolean isPackageDependant(Bundle bundle, String packageName) {
+		BundleWiring wiring = bundle.adapt(BundleWiring.class);
+
+		for (BundleWire wire : wiring.getRequiredWires(null)) {
+			if (wire != null && wire.getCapability().getAttributes().get(BundleRevision.PACKAGE_NAMESPACE) != null && wire.getCapability()
+					.getAttributes().get(BundleRevision.PACKAGE_NAMESPACE).equals(packageName)) {
+				// FIXME use low trace level log or remove this line
+				// System.out.println("MATCH! " + bundle.getSymbolicName() + " === Bundle importing package: " + packageName);
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private static void printMemoryUsage() {
+		Iterator<MemoryPoolMXBean> iter = ManagementFactory.getMemoryPoolMXBeans().iterator();
+		while (iter.hasNext()) {
+			MemoryPoolMXBean item = iter.next();
+			String name = item.getName();
+
+			if (name.equals("PS Perm Gen")) {
+				MemoryType type = item.getType();
+				MemoryUsage usage = item.getUsage();
+				MemoryUsage peak = item.getPeakUsage();
+				MemoryUsage collections = item.getCollectionUsage();
+				System.out
+						.println("Memory usage: Name: " + name + ", type: " + type + ", usage = " + longBytesToMegs(usage.getUsed()) + ", peak = " + longBytesToMegs(peak
+								.getUsed()) + ", collections = " + longBytesToMegs(collections.getUsed()));
+			}
+		}
+	}
+
+	private static String longBytesToMegs(long bytes) {
+		return bytes / (1000 * 1000) + "MB";
 	}
 
 }

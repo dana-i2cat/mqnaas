cc52b9d804d95b9aac5aa71653ae1968690373da
logoff <juliocarlos.barrera@i2cat.net>
2014-04-23 16:20:32 +0200

    Apply code format.

--- core/src/main/java/org/opennaas/core/impl/ApplicationManagement.java
+++ core/src/main/java/org/opennaas/core/impl/ApplicationManagement.java
@@ -1,6 +1,5 @@
 package org.opennaas.core.impl;
 
-import java.util.Collections;
 import java.util.Set;
 
 import org.opennaas.core.api.IApplication;
@@ -9,16 +8,9 @@ import org.osgi.framework.Bundle;
 public class ApplicationManagement {
 
 	public Set<Class<? extends IApplication>> addBundle(Bundle bundle) {
-		// filter to scan only bundles importing target package
-		if (BundleUtils.isPackageDependant(bundle, "org.opennaas.core.api")) {
-			// Scan all class resources of the given bundle and collects all
-			// capability classes, e.g. those which implement ICapability
-			// Scan all class resources of the given bundle and collects all
-			// applications classes, e.g. those which implement IApplication
-			return BundleUtils.scanBundle(bundle, IApplication.class);
-		}
-
-		return Collections.emptySet();
+		// Filter bundles depending on core and scan all class resources of the given bundle and collects all capability classes, e.g. those which
+		// implement IApplication
+		return BundleUtils.scanBundle(bundle, IApplication.class);
 	}
 
 }

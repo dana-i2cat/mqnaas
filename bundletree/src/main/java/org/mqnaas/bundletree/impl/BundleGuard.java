4cfdd67c5471137d348c4e4fd1ec812d3a2ea8af
logoff <juliocarlos.barrera@i2cat.net>
2014-06-05 10:56:56 +0200

    Improve BundleGuard creating ConcurrentBundleGuardData and other minor changes.

--- bundletree/src/main/java/org/mqnaas/bundletree/impl/BundleGuard.java
+++ bundletree/src/main/java/org/mqnaas/bundletree/impl/BundleGuard.java
@@ -1,16 +1,13 @@
 package org.mqnaas.bundletree.impl;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.mqnaas.bundletree.IBundleGuard;
 import org.mqnaas.bundletree.IClassFilter;
 import org.mqnaas.bundletree.IClassListener;
 import org.mqnaas.bundletree.exceptions.BundleNotFoundException;
-import org.mqnaas.bundletree.utils.BundleClassPathUtils;
 import org.mqnaas.bundletree.utils.BundleUtils;
-import org.mqnaas.core.api.ICapability;
 import org.mqnaas.core.api.IRootResource;
 import org.mqnaas.core.api.Specification;
 import org.osgi.framework.Bundle;
@@ -30,97 +27,111 @@ import org.osgi.framework.FrameworkUtil;
  * @author Julio Carlos Barrera
  * 
  */
-public class BundleGuard implements IBundleGuard, BundleListener, ICapability {
+public class BundleGuard implements IBundleGuard {
 
 	// method declaring this instance as MQNaaS core ICapability
 	public static boolean isSupporting(IRootResource resource) {
 		return resource.getSpecification().getType() == Specification.Type.CORE;
 	}
 
-	// map containing relation between classFiletrs and classListeners
-	Map<IClassFilter, IClassListener>	classListenersMap	= new ConcurrentHashMap<IClassFilter, IClassListener>();
-
-	public BundleGuard() {
-		// register itself as BundleListener
-		FrameworkUtil.getBundle(BundleGuard.class).getBundleContext().addBundleListener(this);
-	}
+	InternalBundleListener		bundleListener	= new InternalBundleListener();
+	ConcurrentBundleGuardData	data			= new ConcurrentBundleGuardData();
 
 	@Override
 	public void registerClassListener(IClassFilter classFilter, IClassListener classListener) {
-		// add classFileter-classListener to the map
-		classListenersMap.put(classFilter, classListener);
+		data.registerClassListener(classListener, classFilter);
 	}
 
 	@Override
 	public void unregisterClassListener(IClassListener classListener) {
-		// remove classListener from the map
-		for (Iterator<Map.Entry<IClassFilter, IClassListener>> it = classListenersMap.entrySet().iterator(); it.hasNext();) {
-			if (it.next().getValue().equals(classListener)) {
-				it.remove();
-			}
-		}
+		data.unregisterClassListener(classListener);
 	}
 
-	@Override
-	public void bundleChanged(BundleEvent event) {
-		System.out.println("Received BundleEvent: " + event);
-		// a new bundle got started
-		if (event.getType() == BundleEvent.STARTED) {
-			Bundle bundle = event.getBundle();
+	/*
+	 * Internal BundleListener class
+	 */
+	private class InternalBundleListener implements BundleListener {
 
+		private Set<Bundle>	coreAPIDependentBundles	= new HashSet<Bundle>();
+
+		public InternalBundleListener() {
+			// register itself as BundleListener
+			FrameworkUtil.getBundle(BundleGuard.class).getBundleContext().addBundleListener(this);
+		}
+
+		@Override
+		public void bundleChanged(BundleEvent event) {
+			System.out.println("Received BundleEvent: " + event + ", " + bundleEventType2String(event.getType()));
 			try {
-				// is this bundle dependent of core.api bundle
-				if (BundleUtils.bundleDependsOnBundle(bundle, BundleUtils.getBundleBySymbolicName("core.api"), BundleUtils.LOOK_UP_STRATEGY.UP)) {
-					// scan bundle and notify in case of classFilters matches
-					scanBundleAndNotifyClassFound(bundle);
+				// a new bundle got started
+				if (event.getType() == BundleEvent.STARTED) {
+					Bundle bundle = event.getBundle();
+
+					// is this bundle dependent of core.api bundle
+					if (BundleUtils.bundleDependsOnBundle(bundle, BundleUtils.getBundleBySymbolicName("core.api"), BundleUtils.LOOK_UP_STRATEGY.UP)) {
+						// add this bundle as dependent of core.api
+						coreAPIDependentBundles.add(bundle);
+						// apply bundle IN logic
+						data.bundleIn(bundle);
+					}
+				}
+				// a previous bundle got stopped
+				else if (event.getType() == BundleEvent.STOPPED) {
+					Bundle bundle = event.getBundle();
+
+					// is this bundle dependent of core.api bundle
+					if (coreAPIDependentBundles.contains(bundle)) {
+						// apply bundle OUT logic
+						data.bundleOut(bundle);
+						// remove bundle from coreAPIDependentBundles
+						coreAPIDependentBundles.remove(bundle);
+					}
 				}
 			} catch (BundleNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		// a previous bundle got stopped
-		else if (event.getType() == BundleEvent.STOPPED) {
-			// TODO would be classes available? would we store each notification? :$
-			Bundle bundle = event.getBundle();
-
-			try {
-				// is this bundle dependent of core.api bundle
-				if (BundleUtils.bundleDependsOnBundle(bundle, BundleUtils.getBundleBySymbolicName("core.api"), BundleUtils.LOOK_UP_STRATEGY.UP)) {
-					// scan bundle and notify in case of classFilters matches
-					scanBundleAndNotifyClassLeft(bundle);
-				}
-			} catch (BundleNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				// this should not happen, "core.api" bundle must exist
+				System.out.println("core.api bundle not found: " + e.getMessage());
 			}
 		}
 	}
 
-	private void scanBundleAndNotifyClassFound(Bundle bundle) {
-		// iterate over bundle classes
-		for (Class<?> bundleClass : BundleClassPathUtils.getBundleClasses(bundle)) {
-			// iterate over map asking each classFilter
-			for (IClassFilter classFilter : classListenersMap.keySet()) {
-				if (classFilter.filter(bundleClass)) {
-					// class found, notify classListener
-					classListenersMap.get(classFilter).classEntered(bundleClass);
-				}
-			}
-		}
-	}
-
-	// FIXME this strategy can not be used, uninstalled bundles can not be analysed looking for classes
-	private void scanBundleAndNotifyClassLeft(Bundle bundle) {
-		// iterate over bundle classes
-		for (Class<?> bundleClass : BundleClassPathUtils.getBundleClasses(bundle)) {
-			// iterate over map asking each classFilter
-			for (IClassFilter classFilter : classListenersMap.keySet()) {
-				if (classFilter.filter(bundleClass)) {
-					// class found, notify classListener
-					classListenersMap.get(classFilter).classLeft(bundleClass);
-				}
-			}
+	/*
+	 * Converts integer representation of BundleEvent type attribute to String extracted from <a
+	 * href="http://www.osgi.org/javadoc/r4v43/core/org/osgi/framework/BundleEvent.html">reference</a>
+	 */
+	private static final String bundleEventType2String(int bundleEventType) {
+		switch (bundleEventType) {
+			case BundleEvent.INSTALLED:
+				// The bundle has been installed.
+				return "INSTALLED";
+			case BundleEvent.LAZY_ACTIVATION:
+				// The bundle will be lazily activated
+				return "LAZY_ACTIVATION";
+			case BundleEvent.RESOLVED:
+				// The bundle has been resolved
+				return "RESOLVED";
+			case BundleEvent.STARTED:
+				// The bundle has been started
+				return "STARTED";
+			case BundleEvent.STARTING:
+				// The bundle is about to be activated
+				return "STARTING";
+			case BundleEvent.STOPPED:
+				// The bundle has been stopped
+				return "STOPPED";
+			case BundleEvent.STOPPING:
+				// The bundle is about to deactivated
+				return "STOPPING";
+			case BundleEvent.UNINSTALLED:
+				// The bundle has been uninstalled
+				return "UNINSTALLED";
+			case BundleEvent.UNRESOLVED:
+				// The bundle has been unresolved
+				return "UNRESOLVED";
+			case BundleEvent.UPDATED:
+				// The bundle has been updated
+				return "UPDATED";
+			default:
+				return "UNKNOWN_BUNDLE_EVENT_TYPE";
 		}
 	}
 }

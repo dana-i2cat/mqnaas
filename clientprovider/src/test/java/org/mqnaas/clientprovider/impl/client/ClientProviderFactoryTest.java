50f5c58a733446b3eef38e903a362c535f49ab8b
logoff <juliocarlos.barrera@i2cat.net>
2014-07-24 09:56:20 +0200

    Use helpers in unit test.

--- clientprovider/src/test/java/org/mqnaas/clientprovider/impl/client/ClientProviderFactoryTest.java
+++ clientprovider/src/test/java/org/mqnaas/clientprovider/impl/client/ClientProviderFactoryTest.java
@@ -1,24 +1,21 @@
 package org.mqnaas.clientprovider.impl.client;
 
-import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.Collection;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.mqnaas.bundletree.IBundleGuard;
-import org.mqnaas.bundletree.IClassFilter;
-import org.mqnaas.bundletree.IClassListener;
-import org.mqnaas.clientprovider.api.client.IClientProvider;
-import org.mqnaas.clientprovider.api.client.IInternalClientProvider;
-import org.mqnaas.core.api.Credentials;
 import org.mqnaas.core.api.Endpoint;
-import org.mqnaas.core.api.ILockingBehaviour;
-import org.mqnaas.core.api.IResource;
 import org.mqnaas.core.api.IRootResource;
-import org.mqnaas.core.api.ITransactionBehavior;
-import org.mqnaas.core.api.Specification;
 import org.mqnaas.core.impl.ICoreModelCapability;
+import org.mqnaas.test.helpers.ReflectionTestHelper;
+import org.mqnaas.test.helpers.capability.ArtificialBundleGuard;
+import org.mqnaas.test.helpers.capability.ArtificialCoreModelCapability;
+import org.mqnaas.test.helpers.clientprovider.ClientProviderFactoryHelpers;
+import org.mqnaas.test.helpers.clientprovider.EmptyClient;
+import org.mqnaas.test.helpers.clientprovider.TestClientProvider;
+import org.mqnaas.test.helpers.clientprovider.TestInternalClientProvider;
+import org.mqnaas.test.helpers.resource.TestResourceFactory;
 
 /**
  * Unit tests for {@link ClientProviderFactory}
@@ -31,168 +28,23 @@ public class ClientProviderFactoryTest {
 	@Test
 	public void testClientProviderFactory() throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
 
-		// generate base objects
-		IRootResource resource = generateIRootResource();
-		TestBundleGuard bg = new TestBundleGuard();
+		// generate artificial objects
+		IRootResource resource = TestResourceFactory.generateIRootResource(null, null, null, Arrays.asList(new Endpoint()));
+		ArtificialBundleGuard bg = new ArtificialBundleGuard();
+
+		// create ClientProviderFactory, inject dummy capabilities and start it
 		ClientProviderFactory cpf = new ClientProviderFactory();
-		injectBundleGuard(cpf, bg);
-		injectCoreModelCapability(cpf, new TestCoreModelCapability(resource));
+		ReflectionTestHelper.<ClientProviderFactory, IBundleGuard> injectPrivateField(cpf, bg, "bundleGuard");
+		ReflectionTestHelper.<ClientProviderFactory, ICoreModelCapability> injectPrivateField(cpf, new ArtificialCoreModelCapability(resource),
+				"coreModelCapability");
 		cpf.activate();
 
-		// add two InternalClientProvider's
-		bg.throwClassEntered(getInternalClassListener(cpf), TestInternalClientProvider.class);
+		// add InternalClientProvider
+		bg.throwClassEntered(ClientProviderFactoryHelpers.getInternalClassListener(cpf), TestInternalClientProvider.class);
 
 		// obtain a client from client provider
-		TestClient client = cpf.getClientProvider(TestClientProvider.class).getClient(resource);
-		Assert.assertTrue("Client must be an instance of TestClient.", client instanceof TestClient);
-	}
-
-	/*
-	 * Test IBundleGuard able to be called in order to generate fake events
-	 */
-	private class TestBundleGuard implements IBundleGuard {
-
-		@Override
-		public void activate() {
-			// nothing to do
-		}
-
-		@Override
-		public void deactivate() {
-			// nothing to do
-		}
-
-		@Override
-		public void registerClassListener(IClassFilter classFilter, IClassListener classListener) {
-			// nothing to do
-		}
-
-		@Override
-		public void unregisterClassListener(IClassListener classListener) {
-			// nothing to do
-		}
-
-		public void throwClassEntered(IClassListener classListener, Class<?> clazz) {
-			classListener.classEntered(clazz);
-		}
-
-		public void throwClassLeft(IClassListener classListener, Class<?> clazz) {
-			classListener.classLeft(clazz);
-		}
-
-	}
-
-	/*
-	 * Inject IBundleGuard using reflection
-	 */
-	private static void injectBundleGuard(ClientProviderFactory cpf, IBundleGuard bundleGuard) throws SecurityException, NoSuchFieldException,
-			IllegalArgumentException, IllegalAccessException {
-		Class<ClientProviderFactory> cpfClass = ClientProviderFactory.class;
-		Field bundleGuardField = cpfClass.getDeclaredField("bundleGuard");
-		bundleGuardField.setAccessible(true);
-		bundleGuardField.set(cpf, bundleGuard);
-	}
-
-	/*
-	 * Inject IBundleGuard using reflection
-	 */
-	private static void injectCoreModelCapability(ClientProviderFactory cpf, ICoreModelCapability coreModelCapability) throws SecurityException,
-			NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-		Class<ClientProviderFactory> cpfClass = ClientProviderFactory.class;
-		Field bundleGuardField = cpfClass.getDeclaredField("coreModelCapability");
-		bundleGuardField.setAccessible(true);
-		bundleGuardField.set(cpf, coreModelCapability);
-	}
-
-	/*
-	 * Obtain internalClassListener field using reflection
-	 */
-	private static IClassListener getInternalClassListener(ClientProviderFactory cpf) throws SecurityException, NoSuchFieldException,
-			IllegalArgumentException, IllegalAccessException {
-		Class<ClientProviderFactory> cpfClass = ClientProviderFactory.class;
-		Field internalClassListenerField = cpfClass.getDeclaredField("internalClassListener");
-		internalClassListenerField.setAccessible(true);
-		return (IClassListener) internalClassListenerField.get(cpf);
-	}
-
-	/*
-	 * Test IInternalClientProvider
-	 */
-	public static class TestInternalClientProvider implements IInternalClientProvider<TestClient, TesClientConfiguration> {
-
-		@Override
-		public TestClient getClient(Endpoint ep, Credentials c) {
-			return new TestClient();
-		}
-
-		@Override
-		public TestClient getClient(Endpoint ep, Credentials c, TesClientConfiguration configuration) {
-			return new TestClient();
-		}
-	}
-
-	private interface TestClientProvider extends IClientProvider<TestClient, TesClientConfiguration> {
-
-		@Override
-		public TestClient getClient(IResource resource);
-
-		@Override
-		public TestClient getClient(IResource resource, TesClientConfiguration clientConfiguration);
-
-	}
-
-	private static class TestClient {
-	}
-
-	private class TesClientConfiguration {
-	}
-
-	private static class TestCoreModelCapability implements ICoreModelCapability {
-
-		private IRootResource	resourceToBeReturned;
-
-		public TestCoreModelCapability(IRootResource resourceToBeReturned) {
-			this.resourceToBeReturned = resourceToBeReturned;
-		}
-
-		@Override
-		public void activate() {
-		}
-
-		@Override
-		public void deactivate() {
-		}
-
-		@Override
-		public IRootResource getRootResource(IResource resource) throws IllegalArgumentException {
-			return resourceToBeReturned;
-		}
-
-	}
-
-	private static IRootResource generateIRootResource() {
-		return new IRootResource() {
-
-			@Override
-			public ITransactionBehavior getTransactionBehaviour() {
-				return null;
-			}
-
-			@Override
-			public Specification getSpecification() {
-				return null;
-			}
-
-			@Override
-			public ILockingBehaviour getLockingBehaviour() {
-				return null;
-			}
-
-			@Override
-			public Collection<Endpoint> getEndpoints() {
-				return Arrays.asList(new Endpoint());
-			}
-		};
+		EmptyClient client = cpf.getClientProvider(TestClientProvider.class).getClient(resource);
+		Assert.assertTrue("Client must be an instance of EmptyClient.", client instanceof EmptyClient);
 	}
 
 }

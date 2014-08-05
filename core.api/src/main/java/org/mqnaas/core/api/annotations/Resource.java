001d7fb6219e24119c11a3ff961cf65646c2c18b
logoff <juliocarlos.barrera@i2cat.net>
2014-08-05 14:20:29 +0200

    Remove unused old interface.

--- core.api/src/main/java/org/mqnaas/core/api/annotations/Resource.java
+++ core.api/src/main/java/org/mqnaas/core/api/annotations/Resource.java
@@ -5,13 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.mqnaas.core.api.IBindingManagement;
 import org.mqnaas.core.api.ICapability;
 import org.mqnaas.core.api.IResource;
+import org.mqnaas.core.api.IResourceManagementListener;
 
 /**
  * Annotation to be used in {@link ICapability} instances field of type {@link IResource}.<br />
- * When a new <code>ICapability</code> is instantiated by {@link IBindingManagement}, first field found will be filled by the corresponding
+ * When a new <code>ICapability</code> is instantiated by {@link IResourceManagementListener}, first field found will be filled by the corresponding
  * <code>IResource</code> associated with.
  */
 @Retention(RetentionPolicy.RUNTIME)

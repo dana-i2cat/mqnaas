73d7476b1b1bfb7d988bd398af297a7e6d26df48
adrian-rosello <adrian.rosello@i2cat.net>
2014-12-02 17:51:17 +0100

    Complete capability tests.

--- extensions/network/itests/src/test/java/org/mqnaas/extensions/network/itests/ResourcesIntegrationTest.java
+++ extensions/network/itests/src/test/java/org/mqnaas/extensions/network/itests/ResourcesIntegrationTest.java
@@ -9,14 +9,27 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mqnaas.core.api.Endpoint;
+import org.mqnaas.core.api.IResource;
 import org.mqnaas.core.api.IRootResource;
 import org.mqnaas.core.api.IRootResourceManagement;
 import org.mqnaas.core.api.IServiceProvider;
 import org.mqnaas.core.api.Specification;
 import org.mqnaas.core.api.Specification.Type;
 import org.mqnaas.core.api.exceptions.CapabilityNotFoundException;
+import org.mqnaas.network.api.infrastructure.IInfrastructureAdministration;
 import org.mqnaas.network.api.infrastructure.IInfrastructureProvider;
 import org.mqnaas.network.api.topology.ITopologyProvider;
+import org.mqnaas.network.api.topology.device.IDeviceAdministration;
+import org.mqnaas.network.api.topology.device.IDeviceManagement;
+import org.mqnaas.network.api.topology.device.IPortAdministration;
+import org.mqnaas.network.api.topology.device.IPortManagement;
+import org.mqnaas.network.api.topology.link.ILinkAdministration;
+import org.mqnaas.network.api.topology.link.ILinkManagement;
+import org.mqnaas.network.impl.infrastructure.InfrastructureResource;
+import org.mqnaas.network.impl.topology.TopologyResource;
+import org.mqnaas.network.impl.topology.device.DeviceResource;
+import org.mqnaas.network.impl.topology.device.PortResource;
+import org.mqnaas.network.impl.topology.link.LinkResource;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.CoreOptions;
 import org.ops4j.pax.exam.Option;
@@ -74,11 +87,64 @@ public class ResourcesIntegrationTest {
 
 		IRootResource networkResource = rootResourceMgmt.createRootResource(spec, new ArrayList<Endpoint>());
 
+		// network resource capabilities.
 		ITopologyProvider topologyProviderCapab = serviceProvider.getCapability(networkResource, ITopologyProvider.class);
-		Assert.assertNotNull("Network resource should contain a bounded ITopologyProvider capability.", topologyProviderCapab);
+		Assert.assertNotNull("Network resource should contain a bound ITopologyProvider capability.", topologyProviderCapab);
 
 		IInfrastructureProvider infrastructureProviderCapab = serviceProvider.getCapability(networkResource, IInfrastructureProvider.class);
-		Assert.assertNotNull("Network resource should contain a bounded IInfrastructureProvider capability.", infrastructureProviderCapab);
+		Assert.assertNotNull("Network resource should contain a bound IInfrastructureProvider capability.", infrastructureProviderCapab);
+
+		IResource topology = topologyProviderCapab.getTopology();
+		Assert.assertNotNull("Network should contain a topology, provided by the ITopologyProvider capability.", topology);
+		Assert.assertTrue("TopologyProvider capability should provide topology resources.", topology instanceof TopologyResource);
+
+		IResource infrastructure = infrastructureProviderCapab.getInfrastructure();
+		Assert.assertNotNull("Network should contain an infrastructure, provided by the IInfrastructureProvider capability.", infrastructure);
+		Assert.assertTrue("InfrastructureProvider capability should provide infrastructure resources.",
+				infrastructure instanceof InfrastructureResource);
+
+		// infrastructure resource capabilities
+		IInfrastructureAdministration infrastructureAdminCapab = serviceProvider.getCapability(infrastructure, IInfrastructureAdministration.class);
+		Assert.assertNotNull("Infrastructure resource should contain a bound IInfrastructureAdministration capability.", infrastructureAdminCapab);
+
+		// topology resource capabilities
+		IDeviceManagement deviceManagementCapab = serviceProvider.getCapability(topology, IDeviceManagement.class);
+		Assert.assertNotNull("Topology resource should contain a bound IDeviceManagement capability.", deviceManagementCapab);
+
+		ILinkManagement linkManagementCapab = serviceProvider.getCapability(topology, ILinkManagement.class);
+		Assert.assertNotNull("Topology resource should contain a bound ILinkManagement capability.", linkManagementCapab);
+
+		IPortManagement portManagementCapab = serviceProvider.getCapability(topology, IPortManagement.class);
+		Assert.assertNotNull("Topology resource should contain a bound IPortManagement capability.", portManagementCapab);
+
+		// link resource capabilities
+		IResource link = linkManagementCapab.createLink();
+		Assert.assertNotNull(link);
+		Assert.assertTrue(link instanceof LinkResource);
+
+		ILinkAdministration linkAdminCapab = serviceProvider.getCapability(link, ILinkAdministration.class);
+		Assert.assertNotNull("Link resource should contain a bound ILinkAdministration capability", linkAdminCapab);
+
+		// device resource capabilities
+		IResource device = deviceManagementCapab.createDevice();
+		Assert.assertNotNull(device);
+		Assert.assertTrue(device instanceof DeviceResource);
+
+		IDeviceAdministration deviceAdminCapab = serviceProvider.getCapability(link, IDeviceAdministration.class);
+		Assert.assertNotNull("Device resource should contain a bound IDeviceAdministration capability", deviceAdminCapab);
+
+		// port resource capabilities
+		IResource port = portManagementCapab.createPort();
+		Assert.assertNotNull(port);
+		Assert.assertTrue(port instanceof PortResource);
+
+		IPortAdministration portAdminCapab = serviceProvider.getCapability(link, IPortAdministration.class);
+		Assert.assertNotNull("Device resource should contain a bound IPortAdministration capability", portAdminCapab);
+
+		// remove all resources
+		portManagementCapab.removePort(port);
+		linkManagementCapab.removeLink(link);
+		deviceManagementCapab.removeDevice(device);
 
 		rootResourceMgmt.removeRootResource(networkResource);
 

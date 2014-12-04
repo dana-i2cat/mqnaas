14a1184fce27b4cf029d2165f6dc59397ec96e8a
adrian-rosello <adrian.rosello@i2cat.net>
2014-12-04 11:07:09 +0100

    Remove ISliceAssigment methods from  RequestAdministration.

--- extensions/network/network.impl/src/main/java/org/mqnaas/network/impl/RequestAdministration.java
+++ extensions/network/network.impl/src/main/java/org/mqnaas/network/impl/RequestAdministration.java
@@ -1,51 +1,22 @@
 package org.mqnaas.network.impl;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.mqnaas.core.api.IResource;
 import org.mqnaas.core.api.network.IRequestAdministration;
-import org.mqnaas.core.api.network.ISliceAssignment;
 import org.mqnaas.core.api.network.Period;
 
 /**
- * Implementation of the {@link IRequestAdministration} and
- * {@link ISliceAssignment} capabilities, which is backed by a {@link ConcurrentHashMap} and bound to a
- * {@link RequestResource}.
+ * Implementation of the {@link IRequestAdministration} capabilities, which is bound to a {@link RequestResource}.
  * 
  * @author Georg Mansky-Kummert
  */
-public class RequestAdministration implements IRequestAdministration,
-		ISliceAssignment {
+public class RequestAdministration implements IRequestAdministration
+{
 
 	public static boolean isSupporting(IResource resource) {
 		return resource instanceof RequestResource;
 	}
 
-	private Period period;
-
-	private Map<IResource, IResource> slices;
-
-	@Override
-	public void defineSlice(IResource rootResource, IResource slice) {
-		slices.put(rootResource, slice);
-	}
-
-	@Override
-	public void removeSlice(IResource rootResource) {
-		slices.remove(rootResource);
-	}
-
-	@Override
-	public IResource getSlice(IResource rootResource) {
-		return slices.get(rootResource);
-	}
-
-	@Override
-	public Collection<IResource> getSlices() {
-		return slices.keySet();
-	}
+	private Period	period;
 
 	@Override
 	public void setPeriod(Period period) {
@@ -59,7 +30,6 @@ public class RequestAdministration implements IRequestAdministration,
 
 	@Override
 	public void activate() {
-		slices = new ConcurrentHashMap<IResource, IResource>();
 	}
 
 	@Override

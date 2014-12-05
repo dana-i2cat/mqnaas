12b0b26d5a8d0899f3c0ab2da9cafd350ccf8c43
Georg Mansky-Kummert <georg.mansky-kummert@i2cat.net>
2014-12-05 08:58:29 +0100

    Added IResourceMapping

--- extensions/network/network.api/src/main/java/org/mqnaas/network/api/request/IResourceMapping.java
+++ extensions/network/network.api/src/main/java/org/mqnaas/network/api/request/IResourceMapping.java
@@ -1,7 +1,25 @@
 package org.mqnaas.network.api.request;
 
-import org.mqnaas.core.api.ICapability;
+import java.util.Collection;
 
+import org.mqnaas.core.api.ICapability;
+import org.mqnaas.core.api.IResource;
+import org.mqnaas.core.api.IRootResource;
+
+/**
+ * Maps requested resources to {@link IRootResource}s of the corresponding
+ * network.
+ * 
+ * @author Georg Mansky-Kummert
+ */
 public interface IResourceMapping extends ICapability {
 
+	void defineMapping(IResource requestResource, IRootResource rootResource);
+	
+	IRootResource getMapping(IResource requestResource);
+	
+	void removeMapping(IResource resource);
+	
+	Collection<IResource> getMappedDevices();
+	
 }

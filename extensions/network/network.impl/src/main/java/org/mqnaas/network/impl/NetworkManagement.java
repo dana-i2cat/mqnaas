c4f81f604e970a0075f24e23a75185db5e8d42ac
adrian-rosello <adrian.rosello@i2cat.net>
2014-12-08 12:38:30 +0100

    Remove old network model: topology and infrastructure.

--- extensions/network/network.impl/src/main/java/org/mqnaas/network/impl/NetworkManagement.java
+++ extensions/network/network.impl/src/main/java/org/mqnaas/network/impl/NetworkManagement.java
@@ -1,6 +1,5 @@
 package org.mqnaas.network.impl;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -14,17 +13,10 @@ import org.mqnaas.core.api.IServiceProvider;
 import org.mqnaas.core.api.Specification.Type;
 import org.mqnaas.core.api.annotations.DependingOn;
 import org.mqnaas.core.api.exceptions.CapabilityNotFoundException;
-import org.mqnaas.core.api.slicing.ISlicingCapability;
-import org.mqnaas.network.api.infrastructure.IInfrastructureAdministration;
-import org.mqnaas.network.api.infrastructure.IInfrastructureProvider;
 import org.mqnaas.network.api.request.IRequestBasedNetworkManagement;
-import org.mqnaas.network.api.topology.ITopologyProvider;
-import org.mqnaas.network.api.topology.device.IDeviceAdministration;
-import org.mqnaas.network.api.topology.device.IDeviceManagement;
 import org.mqnaas.network.api.topology.link.ILinkAdministration;
 import org.mqnaas.network.api.topology.link.ILinkManagement;
 import org.mqnaas.network.api.topology.port.IPortAdministration;
-import org.mqnaas.network.api.topology.port.IPortManagement;
 
 /**
  * Manages network resources and provides services to create and release new networks based on network request.
@@ -63,53 +55,56 @@ public class NetworkManagement implements IRequestBasedNetworkManagement {
 		// Manual bind, so that capabilities exist
 		resourceManagementListener.resourceAdded(networkResource, this, IRequestBasedNetworkManagement.class);
 
-		try {
-			TopologyWrapper topology = network.getTopology();
+		// TODO adapt this code to new model, there's no longer topology and infrastructure resources.
 
-			InfrastructureWrapper infrastructure = network.getInfrastructure();
+		// try {
+		//
+		// TopologyWrapper topology = network.getTopology();
+		//
+		// InfrastructureWrapper infrastructure = network.getInfrastructure();
+		//
+		// // Now, add all resources to the network, to the topology and to the
+		// // infrastructure
+		//
+		// RequestWrapper request = new RequestWrapper(requestResource);
+		// for (DeviceWrapper requestedDevice : request.getTopology()
+		// .getDevices()) {
+		//
+		// // Create the device in the network's topology
+		// DeviceWrapper device = topology.createDevice();
+		//
+		// // Obtain the mapping from the request...
+		// IResource requestedResource = request.getInfrastructure()
+		// .getDeviceMapping(requestedDevice);
+		//
+		// // Get the slice, if available...
+		// IResource slice = null;
+		//
+		// IResource resource;
+		//
+		// if (slice != null) {
+		// // Slice definition available. Create a slice of the
+		// // corresponding resource
+		// ISlicingCapability slicingCapability = serviceProvider
+		// .getCapability(requestedResource,
+		// ISlicingCapability.class);
+		//
+		// resource = slicingCapability.createSlice(slice);
+		// } else {
+		// // use the complete resource
+		// // TODO Manage already assigned resources...
+		// resource = requestedResource;
+		// }
+		//
+		// network.addResource(resource);
+		//
+		// infrastructure.defineDeviceMapping(device, resource);
+		// }
 
-			// Now, add all resources to the network, to the topology and to the
-			// infrastructure
-
-			RequestWrapper request = new RequestWrapper(requestResource);
-			for (DeviceWrapper requestedDevice : request.getTopology()
-					.getDevices()) {
-
-				// Create the device in the network's topology
-				DeviceWrapper device = topology.createDevice();
-
-				// Obtain the mapping from the request...
-				IResource requestedResource = request.getInfrastructure()
-						.getDeviceMapping(requestedDevice);
-
-				// Get the slice, if available...
-				IResource slice = null;
-
-				IResource resource;
-
-				if (slice != null) {
-					// Slice definition available. Create a slice of the
-					// corresponding resource
-					ISlicingCapability slicingCapability = serviceProvider
-							.getCapability(requestedResource,
-									ISlicingCapability.class);
-
-					resource = slicingCapability.createSlice(slice);
-				} else {
-					// use the complete resource
-					// TODO Manage already assigned resources...
-					resource = requestedResource;
-				}
-
-				network.addResource(resource);
-
-				infrastructure.defineDeviceMapping(device, resource);
-			}
-
-		} catch (CapabilityNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// } catch (CapabilityNotFoundException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 
 		return networkResource;
 	}
@@ -132,26 +127,6 @@ public class NetworkManagement implements IRequestBasedNetworkManagement {
 
 		public RequestWrapper(IResource request) {
 			this.request = request;
-		}
-
-		public InfrastructureWrapper getInfrastructure()
-				throws CapabilityNotFoundException {
-			if (infrastructureWrapper == null) {
-				infrastructureWrapper = new InfrastructureWrapper(
-						serviceProvider.getCapability(request,
-								IInfrastructureProvider.class)
-								.getInfrastructure());
-			}
-			return infrastructureWrapper;
-		}
-
-		public TopologyWrapper getTopology() throws CapabilityNotFoundException {
-			if (topologyWrapper == null) {
-				topologyWrapper = new TopologyWrapper(serviceProvider
-						.getCapability(request, ITopologyProvider.class)
-						.getTopology());
-			}
-			return topologyWrapper;
 		}
 
 	}
@@ -186,26 +161,6 @@ public class NetworkManagement implements IRequestBasedNetworkManagement {
 					IRootResourceAdministration.class);
 		}
 
-		public TopologyWrapper getTopology() throws CapabilityNotFoundException {
-			if (topologyWrapper == null) {
-				topologyWrapper = new TopologyWrapper(serviceProvider
-						.getCapability(network, ITopologyProvider.class)
-						.getTopology());
-			}
-			return topologyWrapper;
-		}
-
-		public InfrastructureWrapper getInfrastructure()
-				throws CapabilityNotFoundException {
-			if (infrastructureWrapper == null) {
-				infrastructureWrapper = new InfrastructureWrapper(
-						serviceProvider.getCapability(network,
-								IInfrastructureProvider.class)
-								.getInfrastructure());
-			}
-			return infrastructureWrapper;
-		}
-
 	}
 
 	private class TopologyWrapper {
@@ -226,27 +181,6 @@ public class NetworkManagement implements IRequestBasedNetworkManagement {
 			return new LinkWrapper(getLinkManagement().createLink());
 		}
 
-		private IDeviceManagement getDeviceManagement()
-				throws CapabilityNotFoundException {
-			return serviceProvider.getCapability(topology,
-					IDeviceManagement.class);
-		}
-
-		public DeviceWrapper createDevice() throws CapabilityNotFoundException {
-			return new DeviceWrapper(getDeviceManagement().createDevice());
-		}
-
-		public Collection<DeviceWrapper> getDevices()
-				throws CapabilityNotFoundException {
-			List<DeviceWrapper> devices = new ArrayList<DeviceWrapper>();
-
-			for (IResource device : getDeviceManagement().getDevices()) {
-				devices.add(new DeviceWrapper(device));
-			}
-
-			return devices;
-		}
-
 	}
 
 	private class InfrastructureWrapper {
@@ -255,56 +189,6 @@ public class NetworkManagement implements IRequestBasedNetworkManagement {
 
 		public InfrastructureWrapper(IResource infrastructure) {
 			this.infrastructure = infrastructure;
-		}
-
-		public void defineDeviceMapping(DeviceWrapper device, IResource resource)
-				throws CapabilityNotFoundException {
-			getAdministration().defineDeviceMapping(device.getResource(),
-					resource);
-		}
-
-		public IResource getDeviceMapping(DeviceWrapper device)
-				throws CapabilityNotFoundException {
-			return getAdministration().getDeviceMapping(device.getResource());
-		}
-
-		private IInfrastructureAdministration getAdministration()
-				throws CapabilityNotFoundException {
-			return serviceProvider.getCapability(infrastructure,
-					IInfrastructureAdministration.class);
-		}
-
-	}
-
-	private class DeviceWrapper {
-
-		private IResource	device;
-
-		public DeviceWrapper(IResource device) {
-			this.device = device;
-		}
-
-		public IResource getResource() {
-			return device;
-		}
-
-		private IDeviceAdministration getDeviceAdministration()
-				throws CapabilityNotFoundException {
-			return serviceProvider.getCapability(device,
-					IDeviceAdministration.class);
-		}
-
-		public Type getType() throws CapabilityNotFoundException {
-			return getDeviceAdministration().getType();
-		}
-
-		public PortWrapper createPort() throws CapabilityNotFoundException {
-			return new PortWrapper(getPortManagement().createPort());
-		}
-
-		private IPortManagement getPortManagement()
-				throws CapabilityNotFoundException {
-			return serviceProvider.getCapability(device, IPortManagement.class);
 		}
 
 	}

7d51dca341e1a11b576231cb79fc613ffcf2d20e
Georg Mansky-Kummert <georg.mansky-kummert@i2cat.net>
2014-12-10 15:32:38 +0100

    Removed unused imports

--- extensions/network/network.impl/src/main/java/org/mqnaas/network/impl/topology/port/NetworkPortManagement.java
+++ extensions/network/network.impl/src/main/java/org/mqnaas/network/impl/topology/port/NetworkPortManagement.java
@@ -8,7 +8,6 @@ import org.mqnaas.core.api.IResource;
 import org.mqnaas.core.api.IRootResource;
 import org.mqnaas.core.api.Specification.Type;
 import org.mqnaas.network.api.topology.port.INetworkPortManagement;
-import org.mqnaas.network.impl.RequestRootResource;
 
 /**
  * Implementation of the {@link INetworkPortManagement} capability using a {@link CopyOnWriteArrayList}. This implementation is bound to networks.

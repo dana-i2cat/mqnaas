edd098aaa6b692526748c4ce1b5d0fb6ef96aa98
adrian-rosello <adrian.rosello@i2cat.net>
2014-12-11 15:58:51 +0100

    RequestMapping between IResources, no longer using IRootResources.

--- extensions/network/network.impl/src/main/java/org/mqnaas/network/impl/request/Request.java
+++ extensions/network/network.impl/src/main/java/org/mqnaas/network/impl/request/Request.java
@@ -3,7 +3,6 @@ package org.mqnaas.network.impl.request;
 import java.util.Collection;
 
 import org.mqnaas.core.api.IResource;
-import org.mqnaas.core.api.IRootResource;
 import org.mqnaas.core.api.IServiceProvider;
 import org.mqnaas.core.api.exceptions.CapabilityNotFoundException;
 import org.mqnaas.network.api.request.IRequestResourceMapping;
@@ -27,13 +26,13 @@ public class Request {
 	private IRequestResourceMapping getMapping() throws CapabilityNotFoundException {
 		return serviceProvider.getCapability(request, IRequestResourceMapping.class);
 	}
-	
-	private IRootResource getMapping(IResource resource) throws CapabilityNotFoundException {
+
+	private IResource getMapping(IResource resource) throws CapabilityNotFoundException {
 		return getMapping().getMapping(resource);
 	}
 
 	public Collection<IResource> getMappedDevices() throws CapabilityNotFoundException {
 		return getMapping().getMappedDevices();
 	}
-	
+
 }

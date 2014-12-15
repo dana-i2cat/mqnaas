173f9a7334b82ded79868c4c1dd96404b177ed17
Georg Mansky-Kummert <georg.mansky-kummert@i2cat.net>
2014-12-15 11:15:24 +0100

    Implemented isSupporting for RequestRootResource TSON

--- core/src/main/java/org/mqnaas/core/impl/slicing/SliceProvider.java
+++ core/src/main/java/org/mqnaas/core/impl/slicing/SliceProvider.java
@@ -1,5 +1,7 @@
 package org.mqnaas.core.impl.slicing;
 
+import java.lang.reflect.Method;
+
 import org.mqnaas.core.api.IResource;
 import org.mqnaas.core.api.IResourceManagementListener;
 import org.mqnaas.core.api.IRootResource;
@@ -16,25 +18,46 @@ import org.mqnaas.core.api.slicing.ISliceProvider;
  */
 public class SliceProvider implements ISliceProvider {
 
-	private IResource slice;
+	private IResource					slice;
 
 	@DependingOn
-	private IResourceManagementListener resourceManagementListener; 
+	private IResourceManagementListener	resourceManagementListener;
 
 	@Override
 	public IResource getSlice() {
 		return slice;
 	}
-	
+
 	public static boolean isSupporting(IRootResource resource) {
 		return resource.getDescriptor().getSpecification().getType() == Type.TSON;
 	}
-	
+
+	public static boolean isSupporting(IResource resource) {
+		boolean isSupporting = false;
+
+		// FIXME Needs to be refactoring once the binding mechanism was reworked
+		if (resource.getClass().getName().equals("org.mqnaas.network.impl.RequestRootResource")) {
+
+			try {
+				Method m = resource.getClass().getMethod("getType");
+
+				Type type = (Type) m.invoke(resource);
+
+				isSupporting = type.equals(Type.TSON);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		return isSupporting;
+	}
+
 	@Override
 	public void activate() {
 		// TODO: persistence
 		slice = new SliceResource();
-		
+
 		// Add resource manually to the platform
 		resourceManagementListener.resourceAdded(slice, this, ISliceProvider.class);
 	}
@@ -42,7 +65,7 @@ public class SliceProvider implements ISliceProvider {
 	@Override
 	public void deactivate() {
 		// TODO: persistence
-		
+
 		// Remove resource manually from the platform
 		resourceManagementListener.resourceRemoved(slice, this, ISliceProvider.class);
 	}

23631e2e48b3137a37c0ae0be8eb2c96416fd011
adrian-rosello <adrian.rosello@i2cat.net>
2014-12-15 15:32:23 +0100

    Add comments to the RootResourceRequest class.

--- extensions/network/network.impl/src/main/java/org/mqnaas/network/impl/RequestRootResource.java
+++ extensions/network/network.impl/src/main/java/org/mqnaas/network/impl/RequestRootResource.java
@@ -3,25 +3,33 @@ package org.mqnaas.network.impl;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.mqnaas.core.api.IResource;
+import org.mqnaas.core.api.IRootResource;
 import org.mqnaas.core.api.Specification.Type;
 
+/**
+ * <p>
+ * Representation of a {@link IRootResource} inside a {@link Request} resource.
+ * </p>
+ * 
+ * @author Georg Mansky-Kummert
+ */
 public class RequestRootResource implements IResource {
-	
-	private static AtomicInteger ID_COUNTER = new AtomicInteger();
 
-	private String id;
+	private static AtomicInteger	ID_COUNTER	= new AtomicInteger();
 
-	private Type type;
+	private String					id;
+
+	private Type					type;
 
 	public RequestRootResource(Type type) {
 		id = "req-" + ID_COUNTER.incrementAndGet();
 		this.type = type;
 	}
-	
+
 	public Type getType() {
 		return type;
 	}
-	
+
 	@Override
 	public String getId() {
 		return id;

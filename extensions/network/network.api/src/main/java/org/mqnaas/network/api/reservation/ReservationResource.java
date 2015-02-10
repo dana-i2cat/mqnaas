c860a40e99c2c62f7804de0b0c4318976a5f8179
Roselló, Adrián <adrian.rosello@i2cat.net>
2015-02-10 10:52:02 +0100

    Add hashCode, equals and toString methods to Reservation resource.

--- extensions/network/network.api/src/main/java/org/mqnaas/network/api/reservation/ReservationResource.java
+++ extensions/network/network.api/src/main/java/org/mqnaas/network/api/reservation/ReservationResource.java
@@ -34,4 +34,34 @@ public class ReservationResource implements IResource {
 		return id;
 	}
 
+	@Override
+	public String toString() {
+		return "ReservationResource [id=" + id + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ReservationResource other = (ReservationResource) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }

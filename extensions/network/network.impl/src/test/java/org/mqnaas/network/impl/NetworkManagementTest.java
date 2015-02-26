9ca575b8399e50673324f1a9ddc9df1702d4aa26
Roselló, Adrián <adrian.rosello@i2cat.net>
2015-02-26 08:47:04 +0100

    Test for NetworkManagement.defineNetworkPorts method.

--- extensions/network/network.impl/src/test/java/org/mqnaas/network/impl/NetworkManagementTest.java
+++ extensions/network/network.impl/src/test/java/org/mqnaas/network/impl/NetworkManagementTest.java
@@ -19,12 +19,14 @@ import org.mqnaas.general.test.helpers.reflection.ReflectionTestHelper;
 import org.mqnaas.network.api.request.IRequestResourceMapping;
 import org.mqnaas.network.api.topology.link.ILinkAdministration;
 import org.mqnaas.network.api.topology.link.ILinkManagement;
+import org.mqnaas.network.api.topology.port.INetworkPortManagement;
 import org.mqnaas.network.impl.request.Request;
 import org.mqnaas.network.impl.request.RequestResource;
 import org.mqnaas.network.impl.request.RequestResourceMapping;
 import org.mqnaas.network.impl.topology.link.LinkAdministration;
 import org.mqnaas.network.impl.topology.link.LinkManagement;
 import org.mqnaas.network.impl.topology.link.LinkResource;
+import org.mqnaas.network.impl.topology.port.NetworkPortManagement;
 import org.mqnaas.network.impl.topology.port.PortResource;
 import org.powermock.api.mockito.PowerMockito;
 
@@ -117,6 +119,51 @@ public class NetworkManagementTest {
 
 		Assert.assertEquals("Virtual link should contain phyport1 as source port", phyPort1, netLinkAdministration.getSrcPort());
 		Assert.assertEquals("Virtual link should contain phyport2 as destination port", phyPort2, netLinkAdministration.getDestPort());
+
+	}
+
+	@Test
+	public void defineNetworkPortsTest() throws ApplicationActivationException, CapabilityNotFoundException, SecurityException,
+			NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+
+		// create physical port
+		PortResource phyPort = new PortResource();
+		PortResource virtPort = new PortResource();
+
+		// create request network ports
+		INetworkPortManagement reqPortMgmCapab = new NetworkPortManagement();
+		reqPortMgmCapab.activate();
+		reqPortMgmCapab.addPort(virtPort);
+
+		// map ports in request
+		reqMappingCapab.defineMapping(virtPort, phyPort);
+
+		// create networkPortManagement capability
+		INetworkPortManagement netPortMgmCapab = new NetworkPortManagement();
+		netPortMgmCapab.activate();
+
+		// mock service provider to return desired capabilities
+		PowerMockito.when(serviceProvider.getCapability(Mockito.eq(requestResource), Mockito.eq(INetworkPortManagement.class))).thenReturn(
+				reqPortMgmCapab);
+		PowerMockito.when(serviceProvider.getCapability(Mockito.eq(virtualNetwork), Mockito.eq(INetworkPortManagement.class))).thenReturn(
+				netPortMgmCapab);
+		PowerMockito.when(serviceProvider.getCapability(Mockito.eq(requestResource), Mockito.eq(IRequestResourceMapping.class))).thenReturn(
+				reqMappingCapab);
+
+		// assert network has no ports
+		Assert.assertTrue(netPortMgmCapab.getPorts().isEmpty());
+
+		// call method by reflection
+		Method method = networkManagementCapab.getClass().getDeclaredMethod("defineNetworkPorts", IRootResource.class, Request.class);
+		method.setAccessible(true);
+
+		method.invoke(networkManagementCapab, virtualNetwork, request);
+
+		// Assert network has desired port
+		Assert.assertFalse("Virtual network should contain one network port.", netPortMgmCapab.getPorts().isEmpty());
+		Assert.assertEquals("Virtual network should contain one network port.", 1, netPortMgmCapab.getPorts().size());
+
+		Assert.assertEquals("Network port should contain physical port as network port.", phyPort, netPortMgmCapab.getPorts().get(0));
 
 	}
 }
